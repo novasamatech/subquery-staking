46abce74a5afaa0700ff5a96d10463d955252c25
valentun <vallentunn@gmail.com>
2023-03-31 12:03:53 +0700

    Aleph zero APY

--- src/mappings/kusama.ts
+++ src/mappings/kusama.ts
@@ -4,6 +4,6 @@ import {RelaychainRewardCalculator} from "./rewards/Relaychain";
 
 const KUSAMA_GENESIS = "0xb0a8d493285c2df73290dfb7e61f870f17b41801197a149ca93654499ea3dafe"
 
-export async function handleKusamaNewEra(event: SubstrateEvent): Promise<void> {
+export async function handleKusamaNewEra(_: SubstrateEvent): Promise<void> {
     await handleNewEra(await RelaychainRewardCalculator(), KUSAMA_GENESIS)
 }
--- src/mappings/polkadot.ts
+++ src/mappings/polkadot.ts
@@ -4,6 +4,6 @@ import {RelaychainRewardCalculator} from "./rewards/Relaychain";
 
 const POLKADOT_GENESIS = "0x91b171bb158e2d3848fa23a9f1c25182fb8e20313b2c1eb49219da7a70ce90c3"
 
-export async function handlePolkadotNewEra(event: SubstrateEvent): Promise<void> {
+export async function handlePolkadotNewEra(_: SubstrateEvent): Promise<void> {
     await handleNewEra(await RelaychainRewardCalculator(), POLKADOT_GENESIS)
 }
