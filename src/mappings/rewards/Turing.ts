6478715e6d8fa8c359159df51bee949dc7c3dbd6
valentun <vallentunn@gmail.com>
2023-05-22 17:41:11 +0700

    Implement turing reward calculator

---
+++ src/mappings/rewards/Turing.ts
@@ -0,0 +1,21 @@
+import {CollatorStakingRewardCalculator} from "./CollatorStakingRewardCalculator";
+import Big from "big.js";
+import "@oak-network/api-augment"
+import {INumber} from "@polkadot/types-codec/types/interfaces";
+import {BigFromINumber} from "../utils";
+import {Inflation} from "./inflation/Inflation";
+import {EraInfoDataSource} from "../era/EraInfoDataSource";
+
+export class TuringRewardCalculator extends CollatorStakingRewardCalculator {
+
+    constructor(inflation: Inflation, eraInfoDataSource: EraInfoDataSource) {
+        super(inflation, eraInfoDataSource);
+    }
+
+    protected async fetchTotalIssuance(): Promise<Big> {
+        const total = await api.query.balances.totalIssuance()
+        const additional = await api.query.vesting.totalUnvestedAllocation()
+
+        return BigFromINumber(additional as INumber).plus(BigFromINumber(total))
+    }
+}
