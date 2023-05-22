be3596e763ece7f5ec8afd949f546a3fd7f0c2ec
leohar <leohar@rambler.ru>
2023-05-22 17:32:48 +0900

    Add Turing, Calamari, Zeitgeist, Polkadex, Ternoa, Westend projects (#7)
    * add Turing, Calamari, Zeitgeist, Polkadex, Ternoa, Westend projects
    
    * rework handlers
    
    * fix polkadex genesis
    
    * fix dependencies
    
    * adding docker-compose
    
    * fix healthcheck
    
    * fix healthcheck
    
    ---------
    
    Co-authored-by: Stepan Lavrentev <lawrentievsv@gmail.com>

---
+++ src/mappings/ternoa.ts
@@ -0,0 +1,33 @@
+import {SubstrateEvent} from "@subql/types";
+import {handleNewEra} from "./common";
+import {RelaychainRewardCalculator} from "./rewards/Relaychain";
+import {ValidatorEraInfoDataSource} from "./era/ValidatorEraInfoDataSource";
+import {Codec} from "@polkadot/types/types";
+import {INumber} from "@polkadot/types-codec/types/interfaces";
+import {handleRelaychainStakingReward, handleRelaychainStakingSlash} from "./rewards/history/relaychain";
+
+const TERNOA_GENESIS = "0x6859c81ca95ef624c9dfe4dc6e3381c33e5d6509e35e147092bfbc780f777c4e"
+const STAKING_TYPE = "relaychain"
+
+export async function handleTernoaNewEra(_: SubstrateEvent): Promise<void> {
+    let validatorEraInfoDataSource = new ValidatorEraInfoDataSource();
+
+    await handleNewEra(
+        validatorEraInfoDataSource,
+        await RelaychainRewardCalculator(validatorEraInfoDataSource),
+        TERNOA_GENESIS,
+        STAKING_TYPE
+    )
+}
+
+export async function handleTernoaStakingReward(
+    event: SubstrateEvent<[accountId: Codec, reward: INumber]>,
+): Promise<void> {
+    await handleRelaychainStakingReward(event, TERNOA_GENESIS, STAKING_TYPE)
+}
+
+export async function handleTernoaStakingSlash(
+    event: SubstrateEvent<[account: Codec, slash: INumber]>,
+): Promise<void> {
+    await handleRelaychainStakingSlash(event, TERNOA_GENESIS, STAKING_TYPE)
+}
---
+++ src/mappings/zeitgeist.ts
@@ -0,0 +1,27 @@
+import {SubstrateEvent} from "@subql/types";
+import {handleNewEra} from "./common";
+import {ParachainRewardCalculator} from "./rewards/Parachain";
+import {CollatorEraInfoDataSource} from "./era/CollatorEraInfoDataSource";
+import {Codec} from "@polkadot/types/types";
+import {INumber} from "@polkadot/types-codec/types/interfaces";
+import {handleParachainStakingReward} from "./rewards/history/parachain";
+
+const ZEITGEIST_GENESIS = "0x1bf2a2ecb4a868de66ea8610f2ce7c8c43706561b6476031315f6640fe38e060"
+const STAKING_TYPE = "parachain"
+
+export async function handleZeitgeistNewEra(event: SubstrateEvent): Promise<void> {
+    let eraInfoDataSource = new CollatorEraInfoDataSource();
+
+    await handleNewEra(
+        eraInfoDataSource,
+        await ParachainRewardCalculator(eraInfoDataSource),
+        ZEITGEIST_GENESIS,
+        STAKING_TYPE
+    )
+}
+
+export async function handleZeitgeistStakingReward(
+    event: SubstrateEvent<[accountId: Codec, reward: INumber]>,
+): Promise<void> {
+    await handleParachainStakingReward(event, ZEITGEIST_GENESIS, STAKING_TYPE)
+}
