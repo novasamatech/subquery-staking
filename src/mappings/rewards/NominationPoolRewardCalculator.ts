60fc1c954868b260f4e90009afc59a6823ec177f
vovunku <vovunku@mail.ru>
2023-07-27 11:17:38 +0200

    make pool apy working

--- src/mappings/rewards/NominationPoolRewardCalculator.ts
+++ src/mappings/rewards/NominationPoolRewardCalculator.ts
@@ -1,7 +1,10 @@
 import {RewardCalculator} from "./RewardCalculator";
 import {ValidatorStakingRewardCalculator} from "./ValidatorStakingRewardCalculator";
-import { bnToU8a, stringToU8a, u8aConcat } from '@polkadot/util';
+import { bnToU8a, stringToU8a, u8aConcatStrict } from '@polkadot/util';
 import {max} from "../utils";
+import { u8aToHex } from "@polkadot/util";
+import { AccountId32 } from "@polkadot/types/interfaces";
+import { PerbillToNumber } from "../utils"
 
 const EMPTY_H256 = new Uint8Array(32);
 const MOD_PREFIX = stringToU8a('modl');
@@ -19,35 +22,33 @@ export class NominationPoolRewardCalculator implements RewardCalculator {
         const stakersApy = await this.mainRewardCalculator.getStakersApy()
         const pools = await api.query.nominationPools.bondedPools.entries()
         const poolsApy = await Promise.all(pools.map(async ([poolId, poolData]) => {
-            logger.error(`${poolId.args[0].toNumber()}`)
             const poolAddress = this.derivePoolAccount(poolId.args[0].toNumber(), 0)
-            logger.error(`${poolAddress}`)
 
-            logger.error(`${poolData}`)
-            const poolCommission = 0 // poolData.unwrap()["commission"].current ?? 0 
-            logger.error(`${poolCommission}`)
+            const comissionTuple = poolData.unwrap()["commission"]["current"]
+            const poolCommission = comissionTuple.isSome ? PerbillToNumber(comissionTuple.unwrap()[0]) : 0
 
-            const poolActiveNominations = (await api.query.staking.nominators(poolAddress)).unwrap()
-            logger.error(`${poolActiveNominations.targets}`)
-            let poolAPY = max(poolActiveNominations.targets.map((stakeTargetId) => stakersApy[stakeTargetId.toString()]))
-            logger.error(`${poolAPY}`)
+            const poolActiveNominations = (await api.query.staking.nominators(poolAddress))
+            if (poolActiveNominations.isSome) {
+                let poolAPY = max(poolActiveNominations.unwrap().targets.map((stakeTargetId) => stakersApy.get(stakeTargetId.toString()) ?? 0))                
+                return (1 - poolCommission) * poolAPY
+            } else {
+                return 0
+            }
             
-            return (1 - poolCommission) * poolAPY
         }))
 
         return max(poolsApy)
     }
 
-    private derivePoolAccount(poolId: number, accountType: number) : string {
+    private derivePoolAccount(poolId: number, accountType: number) : AccountId32 {
         let palletId = api.consts.nominationPools.palletId.toU8a();
-
-        return api.registry.createType('AccountId32', u8aConcat(
+        return api.registry.createType('AccountId32', u8aToHex(u8aConcatStrict([
             MOD_PREFIX,
             palletId,
             new Uint8Array([accountType]),
             bnToU8a(poolId, U32_OPTS),
             EMPTY_H256
-          )).toString();
+        ])));
     }
     
 }
