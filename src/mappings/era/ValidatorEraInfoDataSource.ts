ddb4e5a699809f67149d53df4d79aaa962a6338a
vovunku <vovunku@mail.ru>
2023-12-27 10:52:53 +0100

    added base implementation to harvest information from new EraStakers interface

--- src/mappings/era/ValidatorEraInfoDataSource.ts
+++ src/mappings/era/ValidatorEraInfoDataSource.ts
@@ -20,6 +20,14 @@ export class ValidatorEraInfoDataSource extends CachingEraInfoDataSource {
 
     protected async fetchEraStakers(): Promise<StakeTarget[]> {
         const era = await this.era()
+        if (api.query.staking.erasStakersOverview) {
+            return await this.fetchEraStakersPaged(era);
+        } else {
+            return await this.fetchEraStakersClipped(era);
+        }
+    }
+
+    private async fetchEraStakersClipped(era: number): Promise<StakeTarget[]> {
         const exposures = await api.query.staking.erasStakersClipped.entries(era)
 
         return exposures.map(([key, exp]) => {
@@ -43,4 +51,41 @@ export class ValidatorEraInfoDataSource extends CachingEraInfoDataSource {
         })
     }
 
+    private async fetchEraStakersPaged(era: number): Promise<StakeTarget[]> {
+        const overview = await api.query.staking.erasStakersOverview.entries(era)
+        const pages = await api.query.staking.erasStakersPaged.entries(era)
+        const exposures = await api.query.staking.erasStakersClipped.entries(era)
+    
+        const othersAggregation = pages.reduce((accumulator, [key, exp]) => {
+            const exposure = (exp as any).unwrap() // TODO: put correct type
+            const [, validatorId] = key.args
+            let validatorAddress = validatorId.toString()
+        
+            const others = exposure.others.map(({who, value}) => {
+                return {
+                    address: who.toString(),
+                    amount: value.toBigInt()
+                }
+            });
+    
+            (accumulator[validatorAddress] = accumulator[validatorAddress] || []).push(...others);
+            console.log(`${validatorId} -- ${accumulator[validatorAddress].length}`)
+            return accumulator;
+        }, {})
+    
+        return overview.map(([key, exp]) => {
+            const exposure = (exp as any).unwrap() // TODO: put correct type
+            const [, validatorId] = key.args
+            let validatorAddress = validatorId.toString()
+    
+            console.log(`${exposure.nominatorCount} -- ${othersAggregation[validatorAddress].length}`)
+    
+            return {
+                address: validatorAddress,
+                selfStake: exposure.own.toBigInt(),
+                totalStake: BigFromINumber(exposure.total),
+                others: othersAggregation[validatorAddress]
+            }
+        });
+    }
 }
