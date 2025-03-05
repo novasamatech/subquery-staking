d767d20a09ba176d4ff34a8868c1fc65ec85660a
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-03-05 15:12:34 +0300

    fix: keep mythos testnet

---
+++ src/mappings/mythos-testnet.ts
@@ -0,0 +1,39 @@
+import {SubstrateEvent} from "@subql/types";
+import {handleNewEra} from "./common";
+import {Codec} from "@polkadot/types/types";
+import {INumber} from "@polkadot/types-codec/types/interfaces";
+import {MythosEraInfoDataSource} from "./era/MythosEraInfoDataSource";
+import {MythosRewardCalculator} from "./rewards/MythosRewardCalculator";
+import {handleReward, RewardArgs} from "./rewards/history/common";
+import {RewardType} from "../types";
+
+const MYTHOS_GENESIS = "0x15f6788bcf1d1a3b7e1c36074584e1a3f3d07e0a46e362a102c3c3df1a93669f"
+const STAKING_TYPE = "mythos"
+
+export async function handleMythosTestnetNewSession(event: SubstrateEvent): Promise<void> {
+    let eraInfoDataSource = new MythosEraInfoDataSource();
+
+    // In mythos, session==era in our terms
+    await handleNewEra(
+        eraInfoDataSource,
+        new MythosRewardCalculator(eraInfoDataSource),
+        MYTHOS_GENESIS,
+        STAKING_TYPE
+    )
+}
+
+export async function handleMythosTestnetStakingReward(
+    event: SubstrateEvent<[accountId: Codec, reward: INumber]>,
+): Promise<void> {
+    const {event: {data: [accountId, amount]}} = event
+
+    const rewardProps: RewardArgs = {
+        amount: amount.toBigInt(),
+        address: accountId.toString(),
+        type: RewardType.reward,
+        chainId: MYTHOS_GENESIS,
+        stakingType: STAKING_TYPE,
+    }
+
+    await handleReward(rewardProps, event)
+}
