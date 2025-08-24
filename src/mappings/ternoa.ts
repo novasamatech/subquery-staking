8de39fe55c3eeda09c7b5b31e7f98a905b1a62a7
Stepan Lavrentev <lawrentievsv@gmail.com>
2025-08-24 17:06:04 +0300

    feat: adding ternoa nom staking

--- src/mappings/ternoa.ts
+++ src/mappings/ternoa.ts
@@ -1,10 +1,11 @@
 import {SubstrateEvent} from "@subql/types";
-import {handleNewEra, handleNewSession} from "./common";
+import {handleNewEra, handleNewSession, POOLED_STAKING_TYPE} from "./common";
 import {RelaychainRewardCalculator} from "./rewards/Relaychain";
 import {ValidatorEraInfoDataSource} from "./era/ValidatorEraInfoDataSource";
 import {Codec} from "@polkadot/types/types";
 import {INumber} from "@polkadot/types-codec/types/interfaces";
 import {handleRelaychainStakingReward, handleRelaychainStakingSlash} from "./rewards/history/relaychain";
+import { handleRelaychainPooledStakingBondedSlash, handleRelaychainPooledStakingReward, handleRelaychainPooledStakingUnbondingSlash } from "./rewards/history/nomination_pools";
 
 const TERNOA_GENESIS = "0x6859c81ca95ef624c9dfe4dc6e3381c33e5d6509e35e147092bfbc780f777c4e"
 const STAKING_TYPE = "relaychain"
@@ -41,4 +42,22 @@ export async function handleTernoaStakingSlash(
     event: SubstrateEvent<[account: Codec, slash: INumber]>,
 ): Promise<void> {
     await handleRelaychainStakingSlash(event, TERNOA_GENESIS, STAKING_TYPE)
+}
+
+export async function handleTernoaPoolStakingReward(
+  event: SubstrateEvent<[accountId: Codec, poolId: INumber, reward: INumber]>,
+): Promise<void> {
+  await handleRelaychainPooledStakingReward(event, TERNOA_GENESIS, POOLED_STAKING_TYPE)
+}
+
+export async function handleTernoaPoolStakingBondedSlash(
+  event: SubstrateEvent<[poolId: INumber, slash: INumber]>,
+): Promise<void> {
+  await handleRelaychainPooledStakingBondedSlash(event, TERNOA_GENESIS, POOLED_STAKING_TYPE)
+}
+
+export async function handleTernoaPoolStakingUnbondingSlash(
+  event: SubstrateEvent<[era: INumber, poolId: INumber, slash: INumber]>,
+): Promise<void> {
+  await handleRelaychainPooledStakingUnbondingSlash(event, TERNOA_GENESIS, POOLED_STAKING_TYPE)
 }
