e7e241b7a698564e4fee8a922b5b99c46bd516ba
Stepan Lavrentev <40560660+stepanLav@users.noreply.github.com>
2025-08-24 17:37:34 +0300

    feat: adding nomination pools (#83)
    * feat: adding nomination pools
    
    * feat: adding ternoa nom staking
    
    * feat: adding polkadex nom pools
    
    * feat: adding joystream nom pools
    
    * feat: add local runner and fix params

--- src/mappings/polkadex.ts
+++ src/mappings/polkadex.ts
@@ -1,10 +1,11 @@
 import {SubstrateEvent} from "@subql/types";
-import {handleNewEra, handleNewSession} from "./common";
+import {handleNewEra, handleNewSession, POOLED_STAKING_TYPE} from "./common";
 import {RelaychainRewardCalculator} from "./rewards/Relaychain";
 import {ValidatorEraInfoDataSource} from "./era/ValidatorEraInfoDataSource";
 import {Codec} from "@polkadot/types/types";
 import {INumber} from "@polkadot/types-codec/types/interfaces";
 import {handleRelaychainStakingReward, handleRelaychainStakingSlash} from "./rewards/history/relaychain";
+import { handleRelaychainPooledStakingBondedSlash, handleRelaychainPooledStakingReward, handleRelaychainPooledStakingUnbondingSlash } from "./rewards/history/nomination_pools";
 
 const POLKADEX_GENESIS = "0x3920bcb4960a1eef5580cd5367ff3f430eef052774f78468852f7b9cb39f8a3c"
 const STAKING_TYPE = "relaychain"
@@ -41,4 +42,22 @@ export async function handlePolkadexStakingSlash(
     event: SubstrateEvent<[account: Codec, slash: INumber]>,
 ): Promise<void> {
     await handleRelaychainStakingSlash(event, POLKADEX_GENESIS, STAKING_TYPE)
+}
+
+export async function handlePolkadexPoolStakingBondedSlash(
+    event: SubstrateEvent<[poolId: INumber, slash: INumber]>,
+): Promise<void> {
+    await handleRelaychainPooledStakingBondedSlash(event, POLKADEX_GENESIS, POOLED_STAKING_TYPE)
+}
+
+export async function handlePolkadexPoolStakingUnbondingSlash(
+    event: SubstrateEvent<[era: INumber, poolId: INumber, slash: INumber]>,
+): Promise<void> {
+    await handleRelaychainPooledStakingUnbondingSlash(event, POLKADEX_GENESIS, POOLED_STAKING_TYPE)
+}
+
+export async function handlePolkadexPoolStakingReward(
+    event: SubstrateEvent<[accountId: Codec, poolId: INumber, reward: INumber]>,
+): Promise<void> {
+    await handleRelaychainPooledStakingReward(event, POLKADEX_GENESIS, POOLED_STAKING_TYPE)
 }
--- src/mappings/vara.ts
+++ src/mappings/vara.ts
@@ -1,10 +1,11 @@
 import {SubstrateEvent} from "@subql/types";
-import {handleNewEra, handleNewSession} from "./common";
+import {handleNewEra, handleNewSession, POOLED_STAKING_TYPE} from "./common";
 import {RelaychainRewardCalculator} from "./rewards/Relaychain";
 import {ValidatorEraInfoDataSource} from "./era/ValidatorEraInfoDataSource";
 import {Codec} from "@polkadot/types/types";
 import {INumber} from "@polkadot/types-codec/types/interfaces";
 import {handleRelaychainStakingReward, handleRelaychainStakingSlash} from "./rewards/history/relaychain";
+import { handleRelaychainPooledStakingBondedSlash, handleRelaychainPooledStakingReward, handleRelaychainPooledStakingUnbondingSlash } from "./rewards/history/nomination_pools";
 
 const VARA_GENESIS = "0xfe1b4c55fd4d668101126434206571a7838a8b6b93a6d1b95d607e78e6c53763"
 const DIRECT_STAKING_TYPE = "relaychain"
@@ -43,4 +44,22 @@ export async function handleVaraStakingSlash(
     event: SubstrateEvent<[account: Codec, slash: INumber]>,
 ): Promise<void> {
     await handleRelaychainStakingSlash(event, VARA_GENESIS, DIRECT_STAKING_TYPE)
+}
+
+export async function handleVaraPoolStakingReward(
+    event: SubstrateEvent<[accountId: Codec, poolId: INumber, reward: INumber]>,
+): Promise<void> {
+    await handleRelaychainPooledStakingReward(event, VARA_GENESIS, POOLED_STAKING_TYPE)
+}
+
+export async function handleVaraPoolStakingBondedSlash(
+    event: SubstrateEvent<[poolId: INumber, slash: INumber]>,
+): Promise<void> {
+    await handleRelaychainPooledStakingBondedSlash(event, VARA_GENESIS, POOLED_STAKING_TYPE)
+}
+
+export async function handleVaraPoolStakingUnbondingSlash(
+    event: SubstrateEvent<[era: INumber, poolId: INumber, slash: INumber]>,
+): Promise<void> {
+    await handleRelaychainPooledStakingUnbondingSlash(event, VARA_GENESIS, POOLED_STAKING_TYPE)
 }
